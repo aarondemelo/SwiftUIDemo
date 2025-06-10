c7203a905d62ad2255a1c263d2fc962dd1fd2d35
aaron-demelo <aarondemelo@gmail.com>
2025-06-10 22:24:13 +0530

    Initial Commit

---
+++ SwiftUIDemoTests/SwiftUIDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoTests.swift
+//  SwiftUIDemoTests
+//
+//  Created by Aaron de Melo on 10/06/25.
+//
+
+import Testing
+@testable import SwiftUIDemo
+
+struct SwiftUIDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
