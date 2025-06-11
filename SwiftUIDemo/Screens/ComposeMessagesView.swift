3a868f1cda3adda84f5dadd8e8907a5fffcb175e
Sanyam Kamat <1625114+sanyamkamat@users.noreply.github.com>
2025-06-11 18:38:12 +0530

    Profile udpates

--- SwiftUIDemo/Screens/ComposeMessagesView.swift
+++ SwiftUIDemo/Screens/ComposeMessagesView.swift
@@ -3,7 +3,7 @@ import SwiftUI
 struct ComposeMessagesView: View {
   @Environment(AppRouter.self) private var router
   var message: Message?
-    
+
   var body: some View {
     Text("Compose Messages")
       .font(.title)
--- SwiftUIDemo/Screens/ProfileView.swift
+++ SwiftUIDemo/Screens/ProfileView.swift
@@ -4,20 +4,39 @@ struct ProfileView: View {
   @Environment(\.appState) var appState
   @EnvironmentObject var themeManager: ThemeManager
   @Environment(\.colorScheme) private var systemColorScheme
-
+  
   var body: some View {
-    Text("Profile")
-      .font(.title)
-      .padding()
-      .foregroundColor(themeManager.current.background)
-    BuilderButton(
-      title: "Logout", size: .large, state: .enabled, type: .primary, iconPosition: nil
-    ) {
-      appState.wrappedValue = .unauthenticated
-      //      themeManager.current =
-      //        themeManager.current == .dark
-      //        ? Theme.light
-      //        : Theme.dark
+    VStack {
+      Text("Profile")
+        .font(.title)
+        .padding()
+        .foregroundColor(themeManager.current.background)
+      BuilderButton(
+        title: "Logout", size: .large, state: .enabled, type: .primary, iconPosition: nil
+      ) {
+        appState.wrappedValue = .unauthenticated
+        //      themeManager.current =
+        //        themeManager.current == .dark
+        //        ? Theme.light
+        //        : Theme.dark
+      }
+      Spacer()
+    }
+    .navigationTitle("Profile")
+    
+  }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+
+  static let themeManager = ThemeManager()
+  static let myRouterObject = AppRouter(initialTab: AppTab.events)
+
+  static var previews: some View {
+    NavigationStack {
+      ProfileView()
+        .environment(myRouterObject)
+        .environmentObject(themeManager)
     }
   }
 }
